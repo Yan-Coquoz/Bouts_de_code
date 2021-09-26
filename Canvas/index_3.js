e67f5596939cd8bb61129d65b49b51a4d362ba23
Yan Coquoz <Yan.Coquoz@gmail.com>
2021-09-26 15:17:22 +0200

    Mise en place du package canvas-sketch

---
+++ Canvas/index_3.js
@@ -0,0 +1,15 @@
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  // les dimensions sont en pixels
+  dimensions: [2048, 2048],
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+  };
+};
+
+canvasSketch(sketch, settings);
